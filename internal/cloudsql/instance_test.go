97cdc0c7f05e27622491fcae46c46338a3ab8124
Eno Compton <enocom@google.com>
2022-03-21 12:53:39 -0600

    Import Cloud SQL Go Connector

---
+++ internal/cloudsql/instance_test.go
@@ -0,0 +1,200 @@
+// Copyright 2020 Google LLC
+
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+
+//     https://www.apache.org/licenses/LICENSE-2.0
+
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package cloudsql
+
+import (
+	"context"
+	"crypto/rand"
+	"crypto/rsa"
+	"errors"
+	"testing"
+	"time"
+
+	"cloud.google.com/go/cloudsqlconn/errtype"
+	"cloud.google.com/go/cloudsqlconn/internal/mock"
+)
+
+// genRSAKey generates an RSA key used for test.
+func genRSAKey() *rsa.PrivateKey {
+	key, err := rsa.GenerateKey(rand.Reader, 2048)
+	if err != nil {
+		panic(err) // unexpected, so just panic if it happens
+	}
+	return key
+}
+
+// RSAKey is used for test only.
+var RSAKey = genRSAKey()
+
+func TestParseConnName(t *testing.T) {
+	tests := []struct {
+		name string
+		want connName
+	}{
+		{
+			"project:region:instance",
+			connName{"project", "region", "instance"},
+		},
+		{
+			"google.com:project:region:instance",
+			connName{"google.com:project", "region", "instance"},
+		},
+		{
+			"project:instance", // missing region
+			connName{},
+		},
+	}
+
+	for _, tc := range tests {
+		c, err := parseConnName(tc.name)
+		if err != nil && tc.want != (connName{}) {
+			t.Errorf("unexpected error: %e", err)
+		}
+		if c != tc.want {
+			t.Errorf("ParseConnName(%s) failed: want %v, got %v", tc.name, tc.want, err)
+		}
+	}
+}
+
+func TestInstanceEngineVersion(t *testing.T) {
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+	tests := []string{
+		"MYSQL_5_7", "POSTGRES_14", "SQLSERVER_2019_STANDARD", "MYSQL_8_0_18",
+	}
+	for _, wantEV := range tests {
+		inst := mock.NewFakeCSQLInstance("my-project", "my-region", "my-instance", mock.WithEngineVersion(wantEV))
+		client, cleanup, err := mock.NewSQLAdminService(
+			ctx,
+			mock.InstanceGetSuccess(inst, 1),
+			mock.CreateEphemeralSuccess(inst, 1),
+		)
+		if err != nil {
+			t.Fatalf("%s", err)
+		}
+		defer func() {
+			if err := cleanup(); err != nil {
+				t.Fatalf("%v", err)
+			}
+		}()
+		i, err := NewInstance("my-project:my-region:my-instance", client, RSAKey, 30*time.Second, nil, "")
+		if err != nil {
+			t.Fatalf("failed to init instance: %v", err)
+		}
+
+		gotEV, err := i.InstanceEngineVersion(ctx)
+		if err != nil {
+			t.Fatalf("failed to retrieve engine version: %v", err)
+		}
+		if wantEV != gotEV {
+			t.Errorf("InstanceEngineVersion(%s) failed: want %v, got %v", wantEV, gotEV, err)
+		}
+
+	}
+}
+
+func TestConnectInfo(t *testing.T) {
+	ctx := context.Background()
+	wantAddr := "0.0.0.0"
+	inst := mock.NewFakeCSQLInstance("my-project", "my-region", "my-instance", mock.WithPublicIP(wantAddr))
+	client, cleanup, err := mock.NewSQLAdminService(
+		ctx,
+		mock.InstanceGetSuccess(inst, 1),
+		mock.CreateEphemeralSuccess(inst, 1),
+	)
+	if err != nil {
+		t.Fatalf("%s", err)
+	}
+	defer func() {
+		if err := cleanup(); err != nil {
+			t.Fatalf("%v", err)
+		}
+	}()
+
+	i, err := NewInstance("my-project:my-region:my-instance", client, RSAKey, 30*time.Second, nil, "")
+	if err != nil {
+		t.Fatalf("failed to create mock instance: %v", err)
+	}
+
+	gotAddr, gotTLSCfg, err := i.ConnectInfo(ctx, PublicIP)
+	if err != nil {
+		t.Fatalf("failed to retrieve connect info: %v", err)
+	}
+
+	if gotAddr != wantAddr {
+		t.Fatalf(
+			"ConnectInfo returned unexpected IP address, want = %v, got = %v",
+			wantAddr, gotAddr,
+		)
+	}
+
+	wantServerName := "my-project:my-region:my-instance"
+	if gotTLSCfg.ServerName != wantServerName {
+		t.Fatalf(
+			"ConnectInfo return unexpected server name in TLS Config, want = %v, got = %v",
+			wantServerName, gotTLSCfg.ServerName,
+		)
+	}
+}
+
+func TestConnectInfoErrors(t *testing.T) {
+	ctx := context.Background()
+
+	client, cleanup, err := mock.NewSQLAdminService(ctx)
+	if err != nil {
+		t.Fatalf("%s", err)
+	}
+	defer cleanup()
+
+	// Use a timeout that should fail instantly
+	im, err := NewInstance("my-project:my-region:my-instance", client, RSAKey, 0, nil, "")
+	if err != nil {
+		t.Fatalf("failed to initialize Instance: %v", err)
+	}
+
+	_, _, err = im.ConnectInfo(ctx, PublicIP)
+	var wantErr *errtype.DialError
+	if !errors.As(err, &wantErr) {
+		t.Fatalf("when connect info fails, want = %T, got = %v", wantErr, err)
+	}
+
+	// when client asks for wrong IP address type
+	gotAddr, _, err := im.ConnectInfo(ctx, PrivateIP)
+	if err == nil {
+		t.Fatalf("expected ConnectInfo to fail but returned IP address = %v", gotAddr)
+	}
+}
+
+func TestClose(t *testing.T) {
+	ctx := context.Background()
+
+	client, cleanup, err := mock.NewSQLAdminService(ctx)
+	if err != nil {
+		t.Fatalf("%s", err)
+	}
+	defer cleanup()
+
+	// Set up an instance and then close it immediately
+	im, err := NewInstance("my-proj:my-region:my-inst", client, RSAKey, 30, nil, "")
+	if err != nil {
+		t.Fatalf("failed to initialize Instance: %v", err)
+	}
+	im.Close()
+
+	_, _, err = im.ConnectInfo(ctx, PublicIP)
+	if !errors.Is(err, context.Canceled) {
+		t.Fatalf("failed to retrieve connect info: %v", err)
+	}
+}
---
+++ internal/mock/sqladmin.go
@@ -0,0 +1,224 @@
+// Copyright 2021 Google LLC
+
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+
+//     https://www.apache.org/licenses/LICENSE-2.0
+
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package mock
+
+import (
+	"context"
+	"crypto/rsa"
+	"crypto/x509"
+	"encoding/json"
+	"encoding/pem"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"net/http/httptest"
+	"sync"
+	"time"
+
+	"google.golang.org/api/option"
+	sqladmin "google.golang.org/api/sqladmin/v1beta4"
+)
+
+// httpClient returns an *http.Client, URL, and cleanup function. The http.Client is
+// configured to connect to test SSL Server at the returned URL. This server will
+// respond to HTTP requests defined, or return a 5xx server error for unexpected ones.
+// The cleanup function will close the server, and return an error if any expected calls
+// weren't received.
+func httpClient(requests ...*Request) (*http.Client, string, func() error) {
+	// Create a TLS Server that responses to the requests defined
+	s := httptest.NewTLSServer(http.HandlerFunc(
+		func(resp http.ResponseWriter, req *http.Request) {
+			for _, r := range requests {
+				if r.matches(req) {
+					r.handle(resp, req)
+					return
+				}
+			}
+			// Unexpected requests should throw an error
+			resp.WriteHeader(http.StatusNotImplemented)
+			// TODO: follow error format better?
+			resp.Write([]byte(fmt.Sprintf("unexpected request sent to mock client: %v", req)))
+		},
+	))
+	// cleanup stops the test server and checks for uncalled requests
+	cleanup := func() error {
+		s.Close()
+		for i, e := range requests {
+			if e.reqCt > 0 {
+				return fmt.Errorf("%d calls left for specified call in pos %d: %v", e.reqCt, i, e)
+			}
+		}
+		return nil
+	}
+
+	return s.Client(), s.URL, cleanup
+
+}
+
+// Request represents a HTTP request for a test Server to mock responses for.
+//
+// Use NewRequest to initialize new Requests.
+type Request struct {
+	sync.Mutex
+
+	reqMethod string
+	reqPath   string
+	reqCt     int
+
+	handle func(resp http.ResponseWriter, req *http.Request)
+}
+
+// matches returns true if a given http.Request should be handled by this Request.
+func (r *Request) matches(hR *http.Request) bool {
+	r.Lock()
+	defer r.Unlock()
+	if r.reqMethod != "" && r.reqMethod != hR.Method {
+		return false
+	}
+	if r.reqPath != "" && r.reqPath != hR.URL.Path {
+		return false
+	}
+	if r.reqCt <= 0 {
+		return false
+	}
+	r.reqCt--
+	return true
+}
+
+// InstanceGetSuccess returns a Request that responds to the `instance.get` SQL Admin
+// endpoint. It responds with a "StatusOK" and a DatabaseInstance object.
+//
+// https://cloud.google.com/sql/docs/mysql/admin-api/rest/v1beta4/instances/get
+func InstanceGetSuccess(i FakeCSQLInstance, ct int) *Request {
+	var ips []*sqladmin.IpMapping
+	for ipType, addr := range i.ipAddrs {
+		if ipType == "PUBLIC" {
+			ips = append(ips, &sqladmin.IpMapping{IpAddress: addr, Type: "PRIMARY"})
+			continue
+		}
+		if ipType == "PRIVATE" {
+			ips = append(ips, &sqladmin.IpMapping{IpAddress: addr, Type: "PRIVATE"})
+		}
+	}
+	certBytes, err := i.signedCert()
+	if err != nil {
+		panic(err)
+	}
+	db := &sqladmin.DatabaseInstance{
+		BackendType:     i.backendType,
+		ConnectionName:  fmt.Sprintf("%s:%s:%s", i.project, i.region, i.name),
+		DatabaseVersion: i.dbVersion,
+		Project:         i.project,
+		Region:          i.region,
+		Name:            i.name,
+		IpAddresses:     ips,
+		ServerCaCert:    &sqladmin.SslCert{Cert: string(certBytes)},
+	}
+
+	r := &Request{
+		reqMethod: http.MethodGet,
+		reqPath:   fmt.Sprintf("/sql/v1beta4/projects/%s/instances/%s", i.project, i.name),
+		reqCt:     ct,
+		handle: func(resp http.ResponseWriter, req *http.Request) {
+			b, err := db.MarshalJSON()
+			if err != nil {
+				http.Error(resp, err.Error(), http.StatusInternalServerError)
+				return
+			}
+			resp.WriteHeader(http.StatusOK)
+			resp.Write(b)
+		},
+	}
+	return r
+}
+
+// CreateEphemeralSuccess returns a Request that responds to the
+// `connect.generateEphemeralCert` SQL Admin endpoint. It responds with a
+// "StatusOK" and a SslCerts object.
+//
+// https://cloud.google.com/sql/docs/mysql/admin-api/rest/v1beta4/connect/generateEphemeralCert
+func CreateEphemeralSuccess(i FakeCSQLInstance, ct int) *Request {
+	r := &Request{
+		reqMethod: http.MethodPost,
+		reqPath:   fmt.Sprintf("/sql/v1beta4/projects/%s/instances/%s:generateEphemeralCert", i.project, i.name),
+		reqCt:     ct,
+		handle: func(resp http.ResponseWriter, req *http.Request) {
+			// Read the body from the request.
+			b, err := ioutil.ReadAll(req.Body)
+			defer req.Body.Close()
+			if err != nil {
+				http.Error(resp, fmt.Errorf("unable to read body: %w", err).Error(), http.StatusBadRequest)
+				return
+			}
+			var eR sqladmin.GenerateEphemeralCertRequest
+			err = json.Unmarshal(b, &eR)
+			if err != nil {
+				http.Error(resp, fmt.Errorf("invalid or unexpected json: %w", err).Error(), http.StatusBadRequest)
+				return
+			}
+			// Extract the certificate from the request.
+			bl, _ := pem.Decode([]byte(eR.PublicKey))
+			if bl == nil {
+				http.Error(resp, fmt.Errorf("unable to decode PublicKey: %w", err).Error(), http.StatusBadRequest)
+				return
+			}
+			pubKey, err := x509.ParsePKIXPublicKey(bl.Bytes)
+			if err != nil {
+				http.Error(resp, fmt.Errorf("unable to decode PublicKey: %w", err).Error(), http.StatusBadRequest)
+				return
+			}
+
+			certBytes, err := i.clientCert(pubKey.(*rsa.PublicKey))
+			if err != nil {
+				http.Error(resp, fmt.Errorf("failed to sign client certificate: %v", err).Error(), http.StatusBadRequest)
+				return
+			}
+
+			// Return the signed cert to the client.
+			c := &sqladmin.SslCert{
+				Cert:           string(certBytes),
+				CommonName:     "Google Cloud SQL Client",
+				CreateTime:     time.Now().Format(time.RFC3339),
+				ExpirationTime: i.Cert.NotAfter.Format(time.RFC3339),
+				Instance:       i.name,
+			}
+			certResp := sqladmin.GenerateEphemeralCertResponse{
+				EphemeralCert: c,
+			}
+			b, err = certResp.MarshalJSON()
+			if err != nil {
+				http.Error(resp, fmt.Errorf("unable to encode response: %w", err).Error(), http.StatusInternalServerError)
+				return
+			}
+			resp.WriteHeader(http.StatusOK)
+			resp.Write(b)
+		},
+	}
+	return r
+}
+
+// NewSQLAdminService creates a SQL Admin API service backed by a mock HTTP
+// backend. Callers should use the cleanup function to close down the server. If
+// the cleanup function returns an error, a caller has not exercised all the
+// registered requests.
+func NewSQLAdminService(ctx context.Context, reqs ...*Request) (*sqladmin.Service, func() error, error) {
+	mc, url, cleanup := httpClient(reqs...)
+	client, err := sqladmin.NewService(
+		ctx,
+		option.WithHTTPClient(mc),
+		option.WithEndpoint(url),
+	)
+	return client, cleanup, err
+}
