45d3ed4f6162adb6640f040f89b98711f152bb02
jackwotherspoon <jackwoth@google.com>
2022-11-28 16:25:29 +0000

    chore: remove testing

--- e2e_test.go
+++ e2e_test.go
@@ -95,10 +95,6 @@ func TestAlloyDBHook(t *testing.T) {
 	if testing.Short() {
 		t.Skip("skipping integration tests")
 	}
-	// throw error
-	if true {
-		panic("THROWING ERROR FOR FLAKYBOT TO CATCH")
-	}
 	testConn := func(db *sql.DB) {
 		var now time.Time
 		if err := db.QueryRow("SELECT NOW()").Scan(&now); err != nil {
