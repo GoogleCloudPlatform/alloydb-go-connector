1b43d59efefc627fc8303b1838d5e9cdd444558a
Eno Compton <enocom@google.com>
2023-10-13 14:22:33 -0600

    chore: keep sample clean

--- e2e_test.go
+++ e2e_test.go
@@ -16,9 +16,13 @@ package alloydbconn_test
 
 import (
 	"context"
+	"database/sql"
+	"fmt"
 	"os"
 	"testing"
 	"time"
+
+	"cloud.google.com/go/alloydbconn/driver/pgxv4"
 )
 
 var (
@@ -73,36 +77,67 @@ func TestPgxConnect(t *testing.T) {
 	}()
 }
 
+// TestDatabaseSQLConnect uses the latest pgx driver under the hood
+// We verify older versions separately below.
 func TestDatabaseSQLConnect(t *testing.T) {
-	tests := []struct {
-		version string
-	}{
-		{
-			version: "v4",
-		},
-		{
-			version: "v5",
-		},
-	}
-
 	if testing.Short() {
 		t.Skip("skipping integration tests")
 	}
 
-	for _, tc := range tests {
-		pool, cleanup, err := connectDatabaseSQL(
-			alloydbInstanceName, alloydbUser, alloydbPass, alloydbDB, tc.version,
-		)
-		if err != nil {
-			_ = cleanup()
-			t.Fatal(err)
-		}
-		defer func() {
-			pool.Close()
-			// best effort
-			_ = cleanup()
-		}()
+	db, cleanup, err := connectDatabaseSQL(
+		alloydbInstanceName, alloydbUser, alloydbPass, alloydbDB,
+	)
+	if err != nil {
+		_ = cleanup()
+		t.Fatal(err)
 	}
+	defer func() {
+		db.Close()
+		// best effort
+		_ = cleanup()
+	}()
+
+	var tt time.Time
+	if err := db.QueryRow("SELECT NOW()").Scan(&tt); err != nil {
+		t.Fatal(err)
+	}
+	t.Log(tt)
+}
+
+func TestDatabaseSQLConnectPGXV4(t *testing.T) {
+	if testing.Short() {
+		t.Skip("skipping integration tests")
+	}
+
+	cleanup, err := pgxv4.RegisterDriver("alloydb-v4")
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	db, err := sql.Open(
+		"alloydb-v4",
+		fmt.Sprintf(
+			// sslmode is disabled, because the Dialer will handle the SSL
+			// connection instead.
+			"host=%s user=%s password=%s dbname=%s sslmode=disable",
+			alloydbInstanceName, alloydbUser, alloydbPass, alloydbDB,
+		),
+	)
+	if err != nil {
+		_ = cleanup()
+		t.Fatal(err)
+	}
+	defer func() {
+		db.Close()
+		// best effort
+		_ = cleanup()
+	}()
+
+	var tt time.Time
+	if err := db.QueryRow("SELECT NOW()").Scan(&tt); err != nil {
+		t.Fatal(err)
+	}
+	t.Log(tt)
 }
 
 func TestDirectDatabaseSQLAutoIAMAuthN(t *testing.T) {
